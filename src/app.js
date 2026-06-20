459cb7780248e8840ce8426fd7b31b5baf3440f9
Satya7250 <satyaprakashdh3134@gamil.com>
2026-06-20 12:16:55 +0530

    cors url fix

--- src/app.js
+++ src/app.js
@@ -14,7 +14,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://fire-ocean.vercel.app",
+    ],
     credentials: true,
   })
 );
